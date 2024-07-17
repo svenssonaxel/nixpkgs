82474a7610944f167ec71dd32f51f8ab4e906c37
Axel Svensson <mail@axelsvensson.com>
2024-07-17 05:59:44 +0000

    pdf-sign: Update and fix

--- pkgs/by-name/pd/pdf-sign/package.nix
+++ pkgs/by-name/pd/pdf-sign/package.nix
@@ -6,22 +6,23 @@
 , ghostscript
 , pdftk
 , poppler_utils
+, which
 , makeBinaryWrapper
 }:
 
 let
   python = python3.withPackages (ps: with ps; [ tkinter ]);
-  binPath = lib.makeBinPath [ ghostscript pdftk poppler_utils ];
+  binPath = lib.makeBinPath [ ghostscript pdftk poppler_utils which ];
 in
 stdenv.mkDerivation {
   pname = "pdf-sign";
-  version = "0-unstable-2023-08-08";
+  version = "0-unstable-2024-07-16";
 
   src = fetchFromGitHub {
     owner = "svenssonaxel";
     repo = "pdf-sign";
-    rev = "98742c6b12ebe2ca3ba375c695f43b52fe38b362";
-    hash = "sha256-5GRk0T1iLqmvWI8zvZE3OWEHPS0/zN/Ie9brjZiFpqc=";
+    rev = "6c373e3df2ac53af74ea84c3b5f299b13d7dae9c";
+    hash = "sha256-yx1ff1JMTydCd5sCIoiT30zRwxNEwFbgEM9++nkJKY4=";
   };
 
   nativeBuildInputs = [ makeBinaryWrapper ];
@@ -31,9 +32,14 @@ stdenv.mkDerivation {
   installPhase = ''
     runHook preInstall
 
-    install -Dm755 pdf-sign pdf-create-empty -t $out/bin
+    sed -ri 's/(^.*argparse\.ArgumentParser.*)\)$/\1, prog="pdf-sign"\)/' pdf-sign
+    sed -ri 's/(^.*argparse\.ArgumentParser.*)\)$/\1, prog="pdf-create-empty"\)/' pdf-create-empty
+    sed -ri 's/(^.*argparse\.ArgumentParser.*)\)$/\1, prog="pdf-from-text"\)/' pdf-from-text
+
+    install -Dm755 pdf-sign pdf-create-empty pdf-from-text -t $out/bin
     wrapProgram $out/bin/pdf-sign --prefix PATH : ${binPath}
     wrapProgram $out/bin/pdf-create-empty --prefix PATH : ${binPath}
+    wrapProgram $out/bin/pdf-from-text --prefix PATH : ${binPath}
 
     runHook postInstall
   '';
